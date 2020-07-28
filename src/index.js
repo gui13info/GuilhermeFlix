73777b75246ec96e2efafd7d20002df972fed671
Guilherme Marques <guilherme013m@gmail.com>
2020-07-28 19:18:18 -0300

    Segunda aula da imersão (Rotas e State)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './Pages/cadastro/video';
+import CadastroCategoria from './Pages/cadastro/Categoria';
+
+//DESAFIO
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+    <Route path="/" component={ Home } exact />
+    <Route path="/cadastro/video" component={ CadastroVideo } />
+    <Route path="/cadastro/categoria" component={ CadastroCategoria } />
+    <Route component={ Pagina404 } />      
+    </Switch>
+
+  </BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
