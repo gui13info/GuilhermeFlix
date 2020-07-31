42215f7e884ce154a4e434af2e1bff77d4dbcf44
Guilherme Marques <guilherme013m@gmail.com>
2020-07-31 03:27:53 -0300

    Teste

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -32,10 +32,8 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080/categorias'
-      : 'https://guilhermeflix.herokuapp.com/categorias';
-       
+      const URL = 'https://guilhermeflix.herokuapp.com/categorias';
+
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
